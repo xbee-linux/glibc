8c13b204a5d4ddae280812b753b68a4a08100054
Eric FISCHER <efischer78@yahoo.fr>
2022-02-23 14:51:48 +0100

    ok

--- templates/patch.sh
+++ templates/patch.sh
@@ -3,4 +3,4 @@ set -e
 
 sed -e '/NOTIFY_REMOVED)/s/)/ \&\& data.attr != NULL)/' -i sysdeps/unix/sysv/linux/mq_notify.c
 patch -Np1 -i ../glibc-2.34-fhs-1.patch
-echo "rootsbindir={{ .pack.xbeeOut }}/usr/sbin" > build/configparms
+echo "rootsbindir=/usr/sbin" > build/configparms
